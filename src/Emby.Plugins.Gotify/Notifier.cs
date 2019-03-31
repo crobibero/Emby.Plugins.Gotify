db7b4101925bf96db65f4b92d24eaf0da6935945
crobibero <cody@robibe.ro>
2019-03-30 20:35:08 -0400

    Initial Commit

---
+++ src/Emby.Plugins.Gotify/Notifier.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Emby.Plugins.Gotify.Configuration;
+using MediaBrowser.Common.Net;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Notifications;
+using MediaBrowser.Model.Logging;
+
+namespace Emby.Plugins.Gotify
+{
+    public class Notifier : INotificationService
+    {
+        private readonly ILogger _logger;
+        private readonly IHttpClient _httpClient;
+
+        public Notifier(ILogManager logManager, IHttpClient httpClient)
+        {
+            _logger = logManager.GetLogger(GetType().Name);
+            _httpClient = httpClient;
+        }
+        
+        public async Task SendNotification(UserNotification request, CancellationToken cancellationToken)
+        {
+            var options = GetOptions(request.User);
+
+            var body = new Dictionary<string, string>();
+            
+            // message parameter is required. If it is sent as null 
+            // put name as message
+
+            if (string.IsNullOrEmpty(request.Description))
+            {
+                body.Add("message", request.Name);
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(request.Name))
+                    body.Add("title", request.Name);
+                body.Add("message", request.Description);
+            }
+            
+            var requestOptions = new HttpRequestOptions
+            {
+                Url = options.Url.TrimEnd('/') + $"/message?token={options.Token}"
+            };
+
+            requestOptions.SetPostData(body);
+
+            await _httpClient.Post(requestOptions).ConfigureAwait(false);
+        }
+
+        public bool IsEnabledForUser(User user)
+        {
+            var options = GetOptions(user);
+            return options != null && IsValid(options) && options.Enabled;
+        }
+        
+        public string Name => Plugin.Instance.Name;
+
+        private static GotifyOptions GetOptions(BaseItem user)
+        {
+            return Plugin.Instance.Configuration.Options
+                .FirstOrDefault(u => string.Equals(u.UserId, user.Id.ToString("N"),
+                    StringComparison.OrdinalIgnoreCase));
+        }
+        
+        private static bool IsValid(GotifyOptions options)
+        {
+            return !string.IsNullOrEmpty(options.Token)
+                   && !string.IsNullOrEmpty(options.Url);
+        }
+    }
+}
---
+++ src/Emby.Plugins.Gotify/Plugin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Emby.Plugins.Gotify.Configuration;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Drawing;
+using MediaBrowser.Model.Plugins;
+using MediaBrowser.Model.Serialization;
+
+namespace Emby.Plugins.Gotify
+{
+    public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages, IHasThumbImage
+    {
+        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer) : base(applicationPaths, xmlSerializer)
+        {
+            Instance = this;
+        }
+
+        public override string Name => "Gotify Notifications";
+        
+        public IEnumerable<PluginPageInfo> GetPages()
+        {
+            return new[]
+            {
+                new PluginPageInfo
+                {
+                    Name = Name,
+                    EmbeddedResourcePath = GetType().Namespace + ".Configuration.config.html"
+                }
+            };
+        }
+
+        public override Guid Id => Guid.Parse("e29621a5-fa9e-4330-982e-ef6e54c0cad2");
+
+        public Stream GetThumbImage()
+        {
+            var type = GetType();
+            return type.Assembly.GetManifestResourceStream(type.Namespace + ".thumb.jpg");
+        }
+
+        public ImageFormat ThumbImageFormat => ImageFormat.Jpg;
+        
+        public static Plugin Instance { get; private set; }
+    }
+}
